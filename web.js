6cd8b2e60ce3d4541341efbc23170c28ac6672af
Georgiy Slobodenyuk <youanden@gmail.com>
2013-07-11 05:11:36 +0000

    Load root url contents from file index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require('express'),
+	fs		= require('fs');
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	response.send( readFromFile('index.html') );
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var readFromFile = function(filename) {
+	return fs.readFileSync(filename, "utf8");
+};
